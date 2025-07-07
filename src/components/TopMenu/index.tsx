d5f8c142abac9227fcd36c71918172585ecca939
喵锵 <mcyyds1234@outlook.com>
2025-07-08 05:39:37 +0800

    修正错误

--- src/components/TopMenu/index.tsx
+++ src/components/TopMenu/index.tsx
@@ -159,7 +159,7 @@ export const TopMenu: FC = () => {
 		store.set(latencyTestDialogAtom, true);
 	}, [store]);
 
-	const onOpenOriginalGitHub = useCallback(async () => {
+	const onOpenGitHub = useCallback(async () => {
 
 		if (import.meta.env.TAURI_ENV_PLATFORM) {
 			await open("https://github.com/Steve-xmh/amll-ttml-tool");
@@ -538,7 +538,7 @@ export const TopMenu: FC = () => {
 								<Trans i18nKey="topBar.menu.help">帮助</Trans>
 							</DropdownMenu.SubTrigger>
 							<DropdownMenu.SubContent>
-								<DropdownMenu.Item onSelect={onOpenOriginalGitHub}>
+								<DropdownMenu.Item onSelect={onOpenGitHub}>
 									原项目 GitHub
 								</DropdownMenu.Item>
 								<DropdownMenu.Item onSelect={onOpenModifiedGitHub}>
